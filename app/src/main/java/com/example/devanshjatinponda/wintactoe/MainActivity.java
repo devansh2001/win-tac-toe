9bfee725ece94a86908b86ea212bca5db4818eba
devansh2001 <dponda3@gatech.edu>
2019-02-18 21:13:32 -0500

    First Commit

--- app/src/main/java/com/example/devanshjatinponda/wintactoe/MainActivity.java
+++ app/src/main/java/com/example/devanshjatinponda/wintactoe/MainActivity.java
@@ -2,15 +2,43 @@ package com.example.devanshjatinponda.wintactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
-    Button buttons[][];
+    private Button buttons[][];
+    private int operated;
+    private boolean humanTurn;
+
     private void clear() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 buttons[i][j].setText("");
+            }
+        }
+    }
+    private boolean isValid(Button button) {
+        return button.getText().equals("");
+    }
+    private boolean isOver() {
+        return operated != 9;
+    }
+    private boolean checkWon() {
+        for (int i = 0; i < 3; i++) {
+                if (buttons[i][0].getText().equals(buttons[i][1].getText())
+                        && buttons[i][0].getText().equals(buttons[i][2].getText())
+                        && !buttons[i][0].equals("")) {
+                    return true;
+                }
+        }
+        for (int i = 0; i < 3; i++) {
+            if (buttons[0][i].getText().equals(buttons[1][i].getText())
+                    && buttons[0][i].getText().equals(buttons[2][i].getText())
+                    && !buttons[0][i].equals("")) {
+                return true;
             }
         }
     }
@@ -37,5 +65,27 @@ public class MainActivity extends AppCompatActivity {
         };
         this.buttons = buttonArray;
         clear();
+
+        Random random = new Random();
+        while (!isOver()) {
+            if (random.nextInt(10) < 5) {
+                humanTurn = true;
+            } else {
+                humanTurn = false;
+            }
+            if (humanTurn) {
+                humanPlay();
+            } else {
+                computerPlay();
+            }
+        }
+    }
+
+    private void humanPlay() {
+
+    }
+
+    private void computerPlay() {
+
     }
 }
